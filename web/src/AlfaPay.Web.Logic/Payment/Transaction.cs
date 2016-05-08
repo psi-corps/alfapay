1594bb3e358b89040c08d070673ff559353d3f64
LoiIver <li-sam@mail.ru>
2016-05-09 00:38:19 +0300

    include AlfaPay.Domain.Processing

---
+++ web/src/AlfaPay.Web.Logic/Payment/Transaction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlfaPay.Web.Frontoffice.Models.Payment
+{
+	 public class Transaction  
+	{
+	 
+		public string Currency { get; set; }
+		public double? Amount { get; set; }
+		public string ClientIP { get; set; }
+		public CardHolder Sender { get; set; }
+
+		public CardHolder Recipient { get; set; }
+
+	}
+}
